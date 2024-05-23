3efe16505aaaf12c16b0e66f060b353bf25df7a8
Akshar Patel <glaivesharp@gmail.com>
2024-05-23 19:20:38 +0530

    Finalize landing page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,43 +1,119 @@
 <script>
 	import '../app.css';
 	import { DarkMode } from 'flowbite-svelte';
-	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
-	import { SalePercentSolid } from 'flowbite-svelte-icons';
+	import {
+		Navbar,
+		NavBrand,
+		NavLi,
+		NavUl,
+		NavHamburger,
+		Footer,
+		FooterCopyright,
+		FooterLinkGroup,
+		FooterBrand,
+		FooterLink,
+		FooterIcon
+	} from 'flowbite-svelte';
+	import { FacebookSolid, GithubSolid, DiscordSolid, TwitterSolid } from 'flowbite-svelte-icons';
 	import { HexagonIcon } from 'lucide-svelte';
-	import { Banner } from 'flowbite-svelte';
 </script>
 
-<Navbar>
-	<NavBrand href="/">
-		<HexagonIcon class="mr-2 h-7 w-7" color="#E03702" strokeWidth="4" />
-		<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
-			>Landing Page</span
-		>
-	</NavBrand>
-	<NavHamburger />
-	<div class="flex">
+<div class="relative px-8">
+	<Navbar
+		class="fixed start-0 top-0 z-20 w-full border-b-2 bg-white/30 px-2 py-2.5 backdrop-blur-md dark:bg-black/30 sm:px-4"
+	>
+		<NavBrand href="/">
+			<HexagonIcon class="mr-2 h-7 w-7" color="#1AA249" strokeWidth="4" />
+			<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
+				>Landing Page</span
+			>
+		</NavBrand>
+		<NavHamburger />
 		<NavUl>
-			<NavLi href="/">Home</NavLi>
+			<NavLi href="/" active={true}>Home</NavLi>
 			<NavLi href="/">About</NavLi>
 			<NavLi href="/">Navbar</NavLi>
 			<NavLi href="/">Pricing</NavLi>
 			<NavLi href="/">Contact</NavLi>
+			<NavLi class="flex"><DarkMode class="px-2 py-0" /></NavLi>
 		</NavUl>
-		<DarkMode />
+	</Navbar>
+	<div class="overflow-scroll pb-16">
+		<slot></slot>
 	</div>
-</Navbar>
-<slot></slot>
-<Banner id="bottom-banner" position="absolute" bannerType="bottom">
-	<p class="flex items-center text-sm font-normal text-gray-500 dark:text-gray-400">
-		<span class="me-3 inline-flex rounded-full bg-gray-200 p-1 dark:bg-gray-600">
-			<SalePercentSolid class="h-4 w-4 text-gray-500 dark:text-gray-400" />
-		</span>
-		<span>
-			Any announcement can go<a
-				href="/"
-				class="ms-0 flex items-center text-sm font-medium text-primary-600 hover:underline dark:text-primary-500 md:ms-1 md:inline-flex"
-				>here</a
-			>
-		</span>
-	</p>
-</Banner>
+</div>
+
+<Footer footerType="sitemap" class="border-t-2 border-gray-700">
+	<div class="flex">
+		<div class="hidden items-center gap-1 self-start px-6 py-8 sm:flex">
+			<HexagonIcon class="h-8 w-8 text-primary-600" strokeWidth="4" />
+			<p class="text-xl font-semibold text-white">Landing Page</p>
+		</div>
+		<div class="grid flex-1 grid-cols-2 gap-8 px-6 py-8 md:grid-cols-4">
+			<div>
+				<h2 class="mb-6 text-sm font-semibold uppercase text-gray-400">Company</h2>
+				<FooterLinkGroup ulClass="text-gray-300">
+					<FooterLink liClass="mb-4" href="/">About</FooterLink>
+					<FooterLink liClass="mb-4" href="/">Careers</FooterLink>
+					<FooterLink liClass="mb-4" href="/">Brand Center</FooterLink>
+					<FooterLink liClass="mb-4" href="/">Blog</FooterLink>
+				</FooterLinkGroup>
+			</div>
+			<div>
+				<h2 class="mb-6 text-sm font-semibold uppercase text-gray-400">Download</h2>
+				<FooterLinkGroup ulClass="text-gray-300">
+					<FooterLink liClass="mb-4" href="/">Discord Server</FooterLink>
+					<FooterLink liClass="mb-4" href="/">Twitter</FooterLink>
+					<FooterLink liClass="mb-4" href="/">Facebook</FooterLink>
+					<FooterLink liClass="mb-4" href="/">Contact Us</FooterLink>
+				</FooterLinkGroup>
+			</div>
+			<div>
+				<h2 class="mb-6 text-sm font-semibold uppercase text-gray-400">Legal</h2>
+				<FooterLinkGroup ulClass="text-gray-300">
+					<FooterLink liClass="mb-4" href="/">Privacy Policy</FooterLink>
+					<FooterLink liClass="mb-4" href="/">Licensing</FooterLink>
+					<FooterLink liClass="mb-4" href="/">Terms & Conditions</FooterLink>
+				</FooterLinkGroup>
+			</div>
+			<div>
+				<h2 class="mb-6 text-sm font-semibold uppercase text-gray-400">Download</h2>
+				<FooterLinkGroup ulClass="text-gray-300">
+					<FooterLink liClass="mb-4" href="/">iOS</FooterLink>
+					<FooterLink liClass="mb-4" href="/">Android</FooterLink>
+					<FooterLink liClass="mb-4" href="/">Windows</FooterLink>
+					<FooterLink liClass="mb-4" href="/">MacOS</FooterLink>
+				</FooterLinkGroup>
+			</div>
+		</div>
+	</div>
+	<div class="bg-gray-700 px-4 py-6 md:flex md:items-center md:justify-between">
+		<FooterCopyright
+			spanClass="text-sm text-gray-300 sm:text-center"
+			href="/"
+			by="Landing Page™"
+		/>
+		<div class="mt-4 flex space-x-6 sm:justify-center md:mt-0 rtl:space-x-reverse">
+			<FooterIcon href="/">
+				<FacebookSolid
+					class="h-5 w-5 text-gray-500 hover:text-gray-900 dark:text-gray-500 dark:hover:text-white"
+				/>
+			</FooterIcon>
+			<FooterIcon href="/">
+				<DiscordSolid
+					class="h-5 w-5 text-gray-500 hover:text-gray-900 dark:text-gray-500 dark:hover:text-white"
+				/>
+			</FooterIcon>
+			<FooterIcon href="/">
+				<TwitterSolid
+					class="h-5 w-5 text-gray-500 hover:text-gray-900 dark:text-gray-500 dark:hover:text-white"
+				/>
+			</FooterIcon>
+			<FooterIcon href="/">
+				<GithubSolid
+					class="h-5 w-5 text-gray-500 hover:text-gray-900 dark:text-gray-500 dark:hover:text-white"
+				/>
+			</FooterIcon>
+		</div>
+	</div>
+</Footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,97 @@
 <script>
-  import { Alert } from 'flowbite-svelte';
+	import { Heading, P, Button, Marquee, Card, Badge } from 'flowbite-svelte';
+	import { ArrowRightOutline } from 'flowbite-svelte-icons';
+	import CustomCard from '../components/card.svelte';
 </script>
 
-<div class="p-8">
-  <Alert>
-    <span class="font-medium">Info alert!</span>
-    Change a few things up and try submitting again.
-  </Alert>
+<div class="mt-16 py-8 lg:p-8">
+	<section class="block lg:flex lg:items-center lg:px-40 lg:py-16">
+		<div class="space-y-5">
+			<div>
+				<Heading>Lessons and insights</Heading>
+				<Heading class="text-primary-600 dark:text-primary-700">from 8 years</Heading>
+				<P class="mt-3 text-pretty text-gray-500 dark:text-gray-400">
+					Where to grow your business as a photographer: site or social media?
+				</P>
+			</div>
+			<Button class="w-[150px] bg-primary-600 font-semibold shadow-lg shadow-green-500/50" size="md"
+				>Register</Button
+			>
+		</div>
+		<img
+			src="images/Illustration.svg"
+			alt="Illustration"
+			class="w-200 h-200 ml-auto hidden dark:brightness-75 lg:block"
+		/>
+	</section>
+
+	<section class="mt-16 flex flex-col items-center gap-2">
+		<p class="text-3xl font-semibold text-gray-700 dark:text-gray-300">Our Clients</p>
+		<P class="text-pretty text-sm text-gray-500 dark:text-gray-400">
+			We have been working with some Fortune 500+ clients
+		</P>
+		<Marquee speed={0.5} class="py-5">
+			<img src="/images/logos/logo-1.svg" alt="Logo 1" class="h-12 w-12 dark:invert" />
+			<img src="/images/logos/logo-2.svg" alt="Logo 2" class="h-12 w-12 dark:invert" />
+			<img src="/images/logos/logo-3.svg" alt="Logo 3" class="h-12 w-12 dark:invert" />
+			<img src="/images/logos/logo-4.svg" alt="Logo 4" class="h-12 w-12 dark:invert" />
+			<img src="/images/logos/logo-5.svg" alt="Logo 5" class="h-12 w-12 dark:invert" />
+			<img src="/images/logos/logo-6.svg" alt="Logo 6" class="h-12 w-12 dark:invert" />
+			<img src="/images/logos/logo-3.svg" alt="Logo 3" class="h-12 w-12 dark:invert" />
+		</Marquee>
+	</section>
+
+	<section class="mt-24 flex flex-col items-center gap-2">
+		<p class="text-center text-3xl font-semibold text-gray-700 dark:text-gray-300">
+			Manage your entire community <br /> in a single system
+		</p>
+		<P class="text-pretty text-sm text-gray-500 dark:text-gray-400">Who are we suitable for?</P>
+		<div class="mt-5 flex flex-col gap-10 sm:flex-row">
+			{#each [{ imgUrl: 'images/system-section/public-icon.svg', title: 'Membership Organisations', description: 'Our membership management software provides full automation of membership renewals and payments' }, { imgUrl: 'images/system-section/institute-icon.svg', title: 'National Associations', description: 'Our membership management software provides full automation of membership renewals and payments' }, { imgUrl: 'images/system-section/club-icon.svg', title: 'Clubs and Groups', description: 'Our membership management software provides full automation of membership renewals and payments' }] as c, i}
+				<CustomCard imageUrl={c.imgUrl} cardTitle={c.title} cardDescription={c.description} />
+			{/each}
+		</div>
+	</section>
+
+	<section class="mt-16 flex items-center gap-10 xl:px-60">
+		<img
+			src="/images/password.svg"
+			width="350"
+			alt="Password"
+			class="hidden dark:brightness-75 lg:block"
+		/>
+		<div class="">
+			<Heading class="text-3xl font-semibold text-gray-700 dark:text-gray-300"
+				>The unseen of spending three years at Pixelgrade</Heading
+			>
+			<P class="mt-3 text-pretty text-gray-500 dark:text-gray-400">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, placeat facilis? Omnis,
+				corporis temporibus nulla officiis autem repellat sit, harum consequuntur eum perferendis
+				expedita, fugiat pariatur ab quo vel facere.
+			</P>
+		</div>
+	</section>
+
+	<section class="mt-16">
+		<Card class="text-center mx-auto" size="lg" padding="xl">
+			<h5
+				class="mb-2 flex items-center justify-center text-3xl font-bold text-gray-900 dark:text-white"
+			>
+				Try our premium service <Badge class="ms-2 font-semibold">PRO</Badge>
+			</h5>
+			<P class="mb-5 text-pretty text-gray-500 dark:text-gray-400">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, placeat facilis? Omnis,
+				corporis temporibus nulla officiis autem repellat sit, harum consequuntur eum perferendis
+				expedita, fugiat pariatur ab quo vel facere.
+			</P>
+			<div
+				class="items-center justify-center space-y-4 sm:flex sm:space-x-4 sm:space-y-0 rtl:space-x-reverse"
+			>
+				<Button class="font-bold shadow-lg shadow-green-500/50">
+					Get a Demo
+					<ArrowRightOutline class="ms-1 h-6 w-6 text-white" />
+				</Button>
+			</div>
+		</Card>
+	</section>
 </div>
-
-
-
